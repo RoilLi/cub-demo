becb42729a5909b745d0b8ba3783b31c93c268d9
Roil.Li <roil830531@gmail.com>
2022-08-01 06:31:21 +0800

    補更多測案

--- src/test/java/com/example/demo/CurrencyControllerTest.java
+++ src/test/java/com/example/demo/CurrencyControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.entity.CurCurrencyNameSetting;
+import com.example.demo.enums.ApiStatusEnum;
 import com.example.demo.mware.coin_desk.client.CoinDeskClient;
 import com.example.demo.mware.coin_desk.model.current_price.CurrentPrice;
 import com.google.gson.Gson;
@@ -39,7 +40,7 @@ public class CurrencyControllerTest {
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     public void testGetCurrencySetting() throws Exception {
         MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/currency/setting"))
                 .andExpect(status().isOk())
@@ -50,7 +51,7 @@ public class CurrencyControllerTest {
 
     @Test
     @Order(1)
-    public void testInsertCurrencySetting() throws Exception {
+    public void testInsertCurrencySettingSuccess() throws Exception {
         CurCurrencyNameSetting curCurrencyNameSetting = new CurCurrencyNameSetting();
         curCurrencyNameSetting.setCurrencyCode("NTD");
         curCurrencyNameSetting.setName("新台幣");
@@ -60,14 +61,30 @@ public class CurrencyControllerTest {
                         .characterEncoding(StandardCharsets.UTF_8)
                         .content(new Gson().toJson(curCurrencyNameSetting)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("returnCode").value("00"))
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.SUCCESS.getCode()))
+                .andReturn().getResponse();
+        System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
+    }
+    @Test
+    @Order(2)
+    public void testInsertCurrencySettingFail() throws Exception {
+        CurCurrencyNameSetting curCurrencyNameSetting = new CurCurrencyNameSetting();
+        curCurrencyNameSetting.setCurrencyCode("NTD");
+        curCurrencyNameSetting.setName("新台幣");
+        curCurrencyNameSetting.setLanguage("TW");
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.post("/api/currency/setting")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(new Gson().toJson(curCurrencyNameSetting)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.FAIL.getCode()))
                 .andReturn().getResponse();
         System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
     }
 
     @Test
-    @Order(3)
-    public void testUpdateCurrencySetting() throws Exception {
+    @Order(4)
+    public void testUpdateCurrencySettingSuccess() throws Exception {
         CurCurrencyNameSetting curCurrencyNameSetting = new CurCurrencyNameSetting();
         curCurrencyNameSetting.setName("新台幣更新");
         MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.patch("/api/currency/setting/4")
@@ -75,34 +92,59 @@ public class CurrencyControllerTest {
                         .characterEncoding(StandardCharsets.UTF_8)
                         .content(new Gson().toJson(curCurrencyNameSetting)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("returnCode").value("00"))
-                .andReturn().getResponse();
-        System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
-    }
-
-    @Test
-    @Order(4)
-    public void testDeleteCurrencySetting() throws Exception {
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.delete("/api/currency/setting/4"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("returnCode").value("00"))
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.SUCCESS.getCode()))
                 .andReturn().getResponse();
         System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
     }
 
     @Test
     @Order(5)
+    public void testUpdateCurrencySettingFail() throws Exception {
+        CurCurrencyNameSetting curCurrencyNameSetting = new CurCurrencyNameSetting();
+        curCurrencyNameSetting.setName("新台幣更新");
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.patch("/api/currency/setting/99")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(new Gson().toJson(curCurrencyNameSetting)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.FAIL.getCode()))
+                .andReturn().getResponse();
+        System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteCurrencySettingSuccess() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.delete("/api/currency/setting/4"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.SUCCESS.getCode()))
+                .andReturn().getResponse();
+        System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @Order(7)
+    public void testDeleteCurrencySettingFail() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.delete("/api/currency/setting/99"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.FAIL.getCode()))
+                .andReturn().getResponse();
+        System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    @Order(8)
     public void testCoinDeskApi() throws Exception {
         CurrentPrice currentPrice = coinDeskClient.getCurrentPrice();
         System.out.println(new Gson().toJson(currentPrice));
     }
 
     @Test
-    @Order(6)
+    @Order(9)
     public void testGetCurrencyInfo() throws Exception {
         MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/currency/info"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("returnCode").value("00"))
+                .andExpect(jsonPath("returnCode").value(ApiStatusEnum.SUCCESS.getCode()))
                 .andReturn().getResponse();
         System.out.println(response.getContentAsString(StandardCharsets.UTF_8));
     }
